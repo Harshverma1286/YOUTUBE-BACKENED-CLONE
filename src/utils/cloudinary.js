54d7ecb927175cbb877bd378b40ab3af21422d6d
Harsh Verma <hv0626266@gmail.com>
2025-06-03 12:52:04 +0530

    so added and learned about multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,23 @@
+const cloudinary = require('cloudinary').v2;
+
+const fs = require('fs');
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:CLOUDINARY_API_SECRET
+})
+
+
+const uploadoncloudinary = async (localfilepath) =>{
+    try{
+        if(!localfilepath) return null;
+        const response = await cloudinary.uploader.upload(localfilepath,{
+            resource_type:"auto"
+        })
+        console.log("file is uploaded on cloudinary",response.url);
+    }catch(error){
+        fs.unlinkSync(localfilepath);
+    }
+}
